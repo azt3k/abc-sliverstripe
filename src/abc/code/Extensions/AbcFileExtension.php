7bb8c4549cb07aa6d3896fe4d3064c051c4d3ac6
azt3k <breaks.nz@gmail.com>
2013-08-28 06:24:01 +1200

    updates to extensions

--- src/abc/code/Extensions/AbcFileExtension.php
+++ src/abc/code/Extensions/AbcFileExtension.php
@@ -2,13 +2,18 @@
 
 class AbcFileExtension extends DataExtension {
 	
+	// this doesn't even work
 	private static $allowed_extensions = array(
 		'','ace','arc','arj','asf','au','avi','bmp','bz2','cab','cda','css','csv','dmg','doc','docx',
 		'flv','f4v','gif','gpx','gz','hqx','htm','html','ico','jar','jpeg','jpg','js','kml', 'm4a','m4v',
 		'mid','midi','mkv','mov','mp3','mp4','mpa','mpeg','mpg','ogg','ogv','pages','pcx','pdf','pkg',
 		'png','pps','ppt','pptx','ra','ram','rm','rtf','sit','sitx','swf','tar','tgz','tif','tiff',
-		'txt','wav','webm','webmv','wma','wmv','xhtml','xls','xlsx','xml','zip','zipx',
+		'ttf','otf','txt','wav','webm','webmv','wma','wmv','xhtml','xls','xlsx','xml','zip','zipx',
 	);
+
+	public static function get_allowed_extensions() {
+		return self::$allowed_extensions;
+	}
 
 	public function getAddFileMimeType(){
 		$return = explode('-', $this->owner->getFileType());
