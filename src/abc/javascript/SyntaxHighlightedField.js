f2d07d4519e25ef4f11607c19c17ecbad0e94f21
azt3k <breaks.nz@gmail.com>
2013-09-01 18:01:07 +1200

    syntax highlighted fixes... yes more

--- src/abc/javascript/SyntaxHighlightedField.js
+++ src/abc/javascript/SyntaxHighlightedField.js
@@ -2,11 +2,13 @@
 	$(document).ready(function(){
 	
 		$('.syntax-highlighted').each(function() {
+
 			var $this = $(this);
-			var myCodeMirror = CodeMirror(this, {
+			var myCodeMirror = CodeMirror.fromTextArea(this, {
 				value: $this.val(),
 				mode:  $this.attr('data-type')
 			});
+
 		});
 
 	});
