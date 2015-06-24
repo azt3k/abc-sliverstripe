9fe69c937595d82d3f29286cb97c0821ad4831c3
Aaron Latham-Ilari <az@dev-io.us>
2015-06-24 15:55:53 +1200

    more improvements to DataObject Search

--- src/abc/code/Classes/DataObjectSearch.php
+++ src/abc/code/Classes/DataObjectSearch.php
@@ -1,6 +1,21 @@
 <?php
 
-class DataObjectSearch {
+class DataObjectSearch extends Object {
+
+    /**
+    * @config
+    */
+    private static $cache_time = null;
+
+    public static function set_cache_time($cache_time) {
+        static::$cache_time = $cache_time;
+    }
+
+    public static function get_cache_time() {
+        $cacheTime = (int) static::$cache_time !== null
+            ? static::$cache_time
+            : Config::inst()->get('DataObjectSearch', 'cache_time');
+    }
 
     protected static function get_blacklisted_words() {
         return array(
@@ -103,110 +118,139 @@ class DataObjectSearch {
      */
     public static function weighted_search($className, $q, array $fields = array('Content' => 1,'Title' => 3), $start = 0, $limit = 10, $filterSql = null) {
 
-        // parse terms
-        // need to analyse various fragments like first 3 words, last 3 words
-        $terms = array_merge(static::str_to_terms($q), static::str_to_fragments($q));
-        $terms[] = $q;
-        $terms = array_unique($terms);
+        // sort out the cache
+        $implodedArgs = $className . '_' . $q . '_' . implode('_', $fields) . '_' . $start . '_' . $limit  . '_' . $filterSql;
+        $cachekey = preg_replace("/[^A-Za-z0-9]/", '_', __FUNCTION__ . "_" . $implodedArgs);
+        $cache = SS_Cache::Factory(
+            'DataObjectSearch',
+            'Output',
+            array(
+                'lifetime' => static::get_cache_time(),
+                'automatic_serialization' => true
+            )
+        );
+        $set = $cache->load($cachekey);
 
-        // Set some vars
-        $set = new ArrayList;
-        $db  = AbcDB::getInstance();
-        $sql = '';
+        // dont hit the db if we don't need to
+        if (!$set) {
 
-        // Fetch Class Data
-        $table = DataObjectHelper::getTableForClass($className);
+            // parse terms
+            // need to analyse various fragments like first 3 words, last 3 words
+            $terms = array_merge(static::str_to_terms($q), static::str_to_fragments($q));
+            $terms[] = $q;
+            $terms = array_unique($terms);
 
-        foreach ($fields as $field => $weight) {
-            foreach ($terms as $term) {
+            // Set some vars
+            $set = new ArrayList;
+            $db  = AbcDB::getInstance();
+            $sql = '';
 
-                // init the recivers
-                $tables = $joins = $filter = array();
+            // expand search to subclasses
+            $classes = array_merge(ClassInfo::subclassesFor($className), array($className));
 
-                // $tables we are working with
-                if ($table) $tables[$table] = $table;
+            // iterate through list
+            foreach ($classes as $className) {
 
-                // Where
-                if ($table) $where[$table][] = $table . ".ClassName = '" . $className . "'";
+                // Fetch Class Data
+                $table = DataObjectHelper::getTableForClass($className);
 
-                // find the table the property is on
-                $extTable = DataObjectHelper::getExtensionTableForClassWithProperty($className, $field);
+                foreach ($fields as $field => $weight) {
+                    foreach ($terms as $term) {
 
-                // join
-                if ( $table && $extTable && $table!=$extTable ) {
-                    $joins[$table][] = $extTable;
-                } elseif ($extTable) {
-                    $tables[$extTable] = $extTable;
-                }
+                        // init the recivers
+                        $tables = $joins = $filter = array();
 
-                // ext table
-                if ($extTable) {
-                    $filter[$table][] = $extTable . "." . $field . " LIKE '%" . Convert::raw2sql($term) . "%'";
-                } else {
-                    $filter[$table][] = $table . "." . $field . " LIKE '%" . Convert::raw2sql($term) . "%'";
-                }
+                        // $tables we are working with
+                        if ($table) $tables[$table] = $table;
 
-                // Build Query
-                foreach($tables as $table){
+                        // Where
+                        if ($table) $where[$table][] = $table . ".ClassName = '" . $className . "'";
 
-                    // Prepare Where Statement
-                    $uWhere     = array_unique($where[$table]);
-                    $uFilter    = array_unique($filter[$table]);
+                        // find the table the property is on
+                        $extTable = DataObjectHelper::getExtensionTableForClassWithProperty($className, $field);
 
-                    // Where SQL
-                    $wSql = "(".implode(' OR ',$uWhere).") AND (".implode(' OR ',$uFilter).")";
+                        // join
+                        if ( $table && $extTable && $table!=$extTable ) {
+                            $joins[$table][] = $extTable;
+                        } elseif ($extTable) {
+                            $tables[$extTable] = $extTable;
+                        }
 
-                    // Make the rest of the SQL
-                    if ($sql) $sql.= " ) UNION ALL ("."\n\n";
-                    $sql.= "SELECT " . $table . ".ClassName, " . $table . ".ID,  " . $weight .  " AS weight" . "\n";
-                    $sql.= "FROM " . $table . "\n";
+                        // ext table
+                        if ($extTable) {
+                            $filter[$table][] = $extTable . "." . $field . " LIKE '%" . Convert::raw2sql($term) . "%'";
+                        } else {
+                            $filter[$table][] = $table . "." . $field . " LIKE '%" . Convert::raw2sql($term) . "%'";
+                        }
 
-                    // join
-                    if (array_key_exists($table, $joins)){
-                        $join = array_unique($joins[$table]);
-                        foreach($join as $j){
-                            $sql .= " LEFT JOIN " . $j . " ON " . $table . ".ID = " . $j . ".ID" . "\n";
+                        // Build Query
+                        foreach($tables as $table){
+
+                            // Prepare Where Statement
+                            $uWhere     = array_unique($where[$table]);
+                            $uFilter    = array_unique($filter[$table]);
+
+                            // Where SQL
+                            $wSql = "(".implode(' OR ',$uWhere).") AND (".implode(' OR ',$uFilter).")";
+
+                            // Make the rest of the SQL
+                            if ($sql) $sql.= " ) UNION ALL ("."\n\n";
+                            $sql.= "SELECT " . $table . ".ClassName, " . $table . ".ID,  " . $weight .  " AS weight" . "\n";
+                            $sql.= "FROM " . $table . "\n";
+
+                            // join
+                            if (array_key_exists($table, $joins)){
+                                $join = array_unique($joins[$table]);
+                                foreach($join as $j){
+                                    $sql .= " LEFT JOIN " . $j . " ON " . $table . ".ID = " . $j . ".ID" . "\n";
+                                }
+                            }
+
+                            // Add the WHERE statement
+                            $sql .= "WHERE " . $wSql . "\n\n";
+                        }
+
+                        // Add Global Filter to Query
+                        if ($filterSql) {
+                            $sql .= (count($tables) == 1 ? "AND " : "WHERE ") . $filterSql;
                         }
                     }
-
-                    // Add the WHERE statement
-                    $sql .= "WHERE " . $wSql . "\n\n";
-                }
-
-                // Add Global Filter to Query
-                if ($filterSql) {
-                    $sql .= (count($tables) == 1 ? "AND " : "WHERE ") . $filterSql;
                 }
             }
+            // Add Limits and order to Query
+            $sql = "
+                SELECT SQL_CALC_FOUND_ROWS ClassName, ID, SUM(weight) AS total_weight
+                FROM ((" . $sql . ")) AS t1
+                GROUP BY ID
+                ORDER BY total_weight DESC
+                LIMIT " . $start . "," . $limit . "
+            ";
+
+            // Get Data
+            // die('<br>' . $sql . '<br>');
+            $result = $db->query($sql);
+            $result = $result ? $result->fetchAll(PDO::FETCH_OBJ) : array() ;
+
+            // Convert to DOs
+            foreach( $result as $entry ){
+
+                // Make the data easier to work with
+                $entry         = (object) $entry;
+                $className     = $entry->ClassName;
+
+                // this is slower, but will be more reliable
+                $dO = DataObject::get_by_id($className, $entry->ID);
+
+                $set->push($dO);
+            }
+
+            // stash total length
+            $set->unlimitedRowCount = $db->query('SELECT FOUND_ROWS() AS total')->fetch(PDO::FETCH_OBJ)->total;
+
+            // cache
+            $cache->save($set, $cachekey);
         }
 
-        // Add Limits and order to Query
-        $sql = "
-            SELECT SQL_CALC_FOUND_ROWS ClassName, ID, SUM(weight) AS total_weight
-            FROM ((" . $sql . ")) AS t1
-            GROUP BY ID
-            ORDER BY total_weight DESC
-            LIMIT " . $start . "," . $limit . "
-        ";
-
-        // Get Data
-        // die('<br>' . $sql . '<br>');
-        $result = $db->query($sql);
-        $result = $result ? $result->fetchAll(PDO::FETCH_OBJ) : array() ;
-
-        // Convert to DOs
-        foreach( $result as $entry ){
-
-            // Make the data easier to work with
-            $entry         = (object) $entry;
-            $className     = $entry->ClassName;
-
-            // this is slower, but will be more reliable
-            $dO = DataObject::get_by_id($className, $entry->ID);
-
-            $set->push($dO);
-        }
-        $set->unlimitedRowCount = $db->query('SELECT FOUND_ROWS() AS total')->fetch(PDO::FETCH_OBJ)->total;
         return $set;
     }
 }
