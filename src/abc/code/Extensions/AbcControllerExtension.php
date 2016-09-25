1314b50b691c44da68af8213f62c03509238093c
Aaron Latham-Ilari <az@dev-io.us>
2016-09-25 15:33:11 +1300

    controller extension

--- src/abc/code/Extensions/AbcControllerExtension.php
+++ src/abc/code/Extensions/AbcControllerExtension.php
@@ -4,10 +4,19 @@
  * @author AzT3K
  */
 class AbcControllerExtension extends Extension {
-	
+
 	public function onAfterInit() {
 		RequirementsHelper::process_requirements();
 	}
 	
-}
+    public function HashedPath($file, $extension = null) {
+        $absPath = Director::getAbsFile(trim($file  . ($extension ? '.' . $extension : ''), '/'));
+        return $file . '?h=' . sha1_file($absPath);
+    }
 
+    public function TimestampedPath($file, $extension = null) {
+        $absPath = Director::getAbsFile(trim($file . ($extension ? '.' . $extension : ''), '/'));
+        return $file . '?m=' . filemtime($absPath);
+    }
+
+}
