5e7cb00b0c8c62bc19ce636e9e48cb5e6d4a10c7
Aaron Latham-Ilari <az@dev-io.us>
2017-01-27 13:27:25 +1300

    exanded filetype list

--- src/abc/code/Extensions/AbcFileExtension.php
+++ src/abc/code/Extensions/AbcFileExtension.php
@@ -1,11 +1,11 @@
 <?php
 
 class AbcFileExtension extends DataExtension {
-	
+
 	// this doesn't even work
 	private static $allowed_extensions = array(
 		'','ace','arc','arj','asf','au','avi','bmp','bz2','cab','cda','css','csv','dmg','doc','docx',
-		'flv','f4v','gif','gpx','gz','hqx','htm','html','ico','jar','jpeg','jpg','js','kml', 'm4a','m4v',
+		'flv','f4v','gif','gpx','gz','hqx','htm','html','ico','jar','jpeg','jpg','js','json','kml', 'm4a','m4v',
 		'mid','midi','mkv','mov','mp3','mp4','mpa','mpeg','mpg','ogg','ogv','pages','pcx','pdf','pkg',
 		'png','pps','ppt','pptx','ra','ram','rm','rtf','sit','sitx','swf','tar','tgz','tif','tiff',
 		'ttf','otf','txt','wav','webm','webmv','wma','wmv','xhtml','xls','xlsx','xml','zip','zipx',
@@ -23,5 +23,5 @@ class AbcFileExtension extends DataExtension {
 	public function getFileSize(){
 		return $this->owner->getSize();
 	}
-	
+
 }
