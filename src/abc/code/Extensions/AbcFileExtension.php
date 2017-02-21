6d17aa21dcb8b6aedb165ccf00087013dbbb96f1
Aaron Latham-Ilari <az@dev-io.us>
2017-02-22 05:11:17 +1300

    updated file whitelist

--- src/abc/code/Extensions/AbcFileExtension.php
+++ src/abc/code/Extensions/AbcFileExtension.php
@@ -5,7 +5,7 @@ class AbcFileExtension extends DataExtension {
 	// this doesn't even work
 	private static $allowed_extensions = array(
 		'','ace','arc','arj','asf','au','avi','bmp','bz2','cab','cda','css','csv','dmg','doc','docx',
-		'flv','f4v','gif','gpx','gz','hqx','htm','html','ico','jar','jpeg','jpg','js','json','kml', 'm4a','m4v',
+		'flv','f4v','gif','gpx','gz','hqx','htm','html','ico','jar','jpeg','jpg','js','json','kml', 'log', 'm4a','m4v',
 		'mid','midi','mkv','mov','mp3','mp4','mpa','mpeg','mpg','ogg','ogv','pages','pcx','pdf','pkg',
 		'png','pps','ppt','pptx','ra','ram','rm','rtf','sit','sitx','swf','tar','tgz','tif','tiff',
 		'ttf','otf','txt','wav','webm','webmv','wma','wmv','xhtml','xls','xlsx','xml','zip','zipx',
